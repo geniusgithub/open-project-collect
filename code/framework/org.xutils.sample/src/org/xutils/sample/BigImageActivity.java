4fdcb36b263f6515adf9222cbcd8d0cb2a6562b9
geniusgithub <lance.wyj@gmail.com>
2015-11-06 20:42:11 +0800

    update xutils project

---
+++ code/framework/org.xutils.sample/src/org/xutils/sample/BigImageActivity.java
@@ -0,0 +1,33 @@
+package org.xutils.sample;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+import org.xutils.image.ImageOptions;
+import org.xutils.x;
+
+@ContentView(R.layout.activity_big_image)
+public class BigImageActivity extends BaseActivity {
+
+    @ViewInject(R.id.iv_big_img)
+    private ImageView iv_big_img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ImageOptions imageOptions = new ImageOptions.Builder()
+                // 加载中或错误图片的ScaleType
+                //.setPlaceholderScaleType(ImageView.ScaleType.MATRIX)
+                // 默认自动适应大小
+                // .setSize(...)
+                .setIgnoreGif(false)
+                .setImageScaleType(ImageView.ScaleType.MATRIX).build();
+
+        x.image().bind(iv_big_img, getIntent().getStringExtra("url"), imageOptions);
+        //x.image().bind(iv_big_img, "file:///sdcard/test.gif", imageOptions);
+        //x.image().bind(iv_big_img, "assets://test.gif", imageOptions);
+    }
+}
